1b11eabc8cb9a6ef16b923a515fd06eae8864174
moa3002 <moatasemmarwan04@gmail.com>
2023-07-13 10:59:32 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,6 @@
 */
 int main(void)
 {
-<<<<<<< HEAD
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
-=======
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
->>>>>>> b85d01704f1d0f5bc700d7b9f78eb0b3aa2a282f
+return (0);
 }
