44d483a53223cbf7ea817c512614118e07eb1a14
moa3002 <moatasemmarwan04@gmail.com>
2023-07-13 11:27:42 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /* Include the library
  * Standard input/output
+ * main: entry point
+ * return: always 0 (success)
  */
 
 int main(void)
-/*
-*main:  entry point
-*return: always 0 (success)
-*/
-
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
