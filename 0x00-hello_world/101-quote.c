4753a5d569caf33feedf8a3a0bc60d354e990618
moa3002 <moatasemmarwan04@gmail.com>
2023-07-13 16:20:54 -0700

    Advanced 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * followed by a new line, to the standard error
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 1
+*/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
