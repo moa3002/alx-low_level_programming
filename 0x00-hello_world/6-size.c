9d89bc6930499613c2a937ca5734fccbc59db488
moa3002 <moatasemmarwan04@gmail.com>
2023-07-13 12:37:22 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - Prints the size of various typesa based on
-*the computer it is compiled and run on..
+*the computer it is compiled and runvon..
 *Return: Always 0.
 */
 int main(void)
