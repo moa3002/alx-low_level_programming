df968790e23829038ec2800b52a1fdcae3bb4b06
moa3002 <moatasemmarwan04@gmail.com>
2023-07-18 18:33:17 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,8 +7,8 @@
 
 /**
  * add - Adds two integers.
- * a: The first integer to be added.
- * b: The second integer to be added.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
  *
  * Return: The result of the addition.
  */
