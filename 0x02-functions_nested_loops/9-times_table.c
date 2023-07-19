bfedbb0e1f2c3d102e3342384c03f318eccd4512
moa3002 <moatasemmarwan04@gmail.com>
2023-07-18 17:47:54 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,9 @@
 
 
 	/**
-	 *times_table - prints the 9 times table, starting with 0
+	 * times_table - prints the 9 times table, starting with 0
 	 */
-	void times_table(void)
+	void times_table(void);
 	{
 		int i, j, k;
 
