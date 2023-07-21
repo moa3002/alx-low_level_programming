393b96131381bb3e163ef72e8e5605547998d20d
moa3002 <moatasemmarwan04@gmail.com>
2023-07-21 03:04:06 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+int _isupper(int c);
 int main(void)
 {
     char c;
