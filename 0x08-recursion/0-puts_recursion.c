bb56c22b6c782b36315e21579292cca70b721572
moa3002 <moatasemmarwan04@gmail.com>
2023-08-02 05:12:56 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,16 @@
  *  * _puts_recursion - function like puts();
  *   * @s: input
  *    * Return: Always 0 (Success)
- *     */
+ *     
+ */
 void _puts_recursion(char *s)
 {
-		if (*s)
+if (*s)
 				{
 							_putchar(*s);
 									_puts_recursion(s + 1);
 										}
 
-			else
+else
 						_putchar('\n');
 }
