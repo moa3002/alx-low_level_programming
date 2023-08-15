bed01461a5e8da6a0f7238c947d2848c460a5605
moa3002 <moatasemmarwan04@gmail.com>
2023-08-15 02:56:14 +0200

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - a dog's basic info
-* @name: First member
-* @age: Second member
-* @owner: Third member
-* Description: Longer description
+*struct dog - a dog's basic info
+*@name: First member
+*@age: Second member
+*@owner: Third member
+*Description: Longer description
 */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
-}
+char *name;
+float age;
+char *owner;
+};
 /**
 * dog_t - typedef for struct dog
 */
